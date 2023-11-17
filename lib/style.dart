ee2ed3fd9d1773ab89531a725f8f44550f293774
tbrivan <tbrivan22@gmail.com>
2023-11-17 15:38:33 +0700

    finish

---
+++ lib/style.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const largeTextSize = 26.0;
+const mediumTextSize = 20.0;
+const bodyTextSize = 16.0;
+
+const String fontNameDefault = 'Montserrat';
+
+const titleAppTextStyle = TextStyle(
+  fontFamily: fontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: mediumTextSize,
+  color: Colors.white,
+);
+
+const titleTextStyle = TextStyle(
+    fontFamily: fontNameDefault,
+    fontWeight: FontWeight.w300,
+    fontSize: largeTextSize,
+    color: Colors.black);
+
+const body1TextStyle = TextStyle(
+    fontFamily: fontNameDefault,
+    fontWeight: FontWeight.w300,
+    fontSize: bodyTextSize,
+    color: Colors.black);
